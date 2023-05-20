7ab60f7de95122c035bf86fd9788578d715c990c
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-05-20 11:10:18 -0500

    fixed the nested loop!

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const input = "I hate it here.";
+const input = "i hate it here.";
 const vowels = ["a", "e", "i", "o", "u"];
 const resultArray = [];
 
@@ -9,9 +9,9 @@ for (let i = 0; i < input.length; i++) {
   //this nested loop compares input against vowels
   for (let j = 0; j < vowels.length; j++) {
     /* console.log(j); */
-    if (i === j) {
-        console.log(input[i]);
-        //output is incorrect: "I hat" doesn't match vowels array
+    if (input[i] === vowels[j]) {
+      /* console.log(input[i]);
+      important note: this log will not include a capital "I" bc the if statement uses === and requires an exact match*/
     }
   }
 }
